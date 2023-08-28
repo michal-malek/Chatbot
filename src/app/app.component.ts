8cec85dc23f46e987e57e6d73badbbdda361c1bf
Michał Małek <michal.malek@kwsa.pl>
2023-08-28 13:35:12 +0200

    from work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,17 +39,17 @@ export class AppComponent implements OnInit {
     //   js.src = "https://widget.tiledesk.com/v4/launch.js";
     //   fjs.parentNode.insertBefore(js, fjs);
     // }(document, 'script', 'tiledesk-jssdk'));
-  
-    this.window.tiledeskSettings = 
-      {
-      projectid: "64799b3d52caaa00131358b5"
-      };
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "https://chatbot.pgg.pl/widget/launch.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'tiledesk-jssdk'));
+
+    this.window.tiledeskSettings =
+    {
+      projectid: "64ec6f0e5fe201001a936085"
+    };
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://chatbot.pgg.pl/widget/launch.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'tiledesk-jssdk'));
   }
 }
